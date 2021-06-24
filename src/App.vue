fb09ae7a36e5f7e8fb35aedc94ea7f7dd7c05705
Rag1995 <taipeiyyy@gmail.com>
2021-06-24 23:18:21 +0800

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div ref="app" id="app">
+    <navbar />
+    <main>
+      <transition :name="transitionName">
+        <router-view />
+      </transition>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import screenfull from "screenfull";
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      transitionName: "",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    navbar: () => import("@/components/navbar.vue"),
+  },
+  watch: {
+    $route(to, from) {
+      if (to.meta.index > from.meta.index) {
+        this.transitionName = "slide-left";
+      } else {
+        this.transitionName = "slide-right";
+      }
+    },
+  },
+  methods: {
+    click() {
+      if (screenfull.isEnabled) {
+        screenfull.request(document.getElementById("app"));
+      }
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass" scoped>
+#app
+  background-color: #EEEEEE
+main
+  padding: 1.5rem 0
+  text-align: center
+
+// vue transition life circle
+
+// v-leave          離開 前
+// v-leave-active   離開 ing
+// v-leave-to       離開 後
+
+// v-enter          進去 前    
+// v-enter-active   進去 ing
+// v-enter-to       進去 後
+
+.transition
+  overflow: hidden
+
+.slide-left,
+.slide-right
+  &-enter-active,
+  &-leave-active
+    transition: all .5s ease-in-out
+    position: fixed
+    min-width: 100vw
+
+.slide-left-enter,
+.slide-right-leave-to
+  transform: translateX(100%)
+
+.slide-left-leave-to,
+.slide-right-enter
+  transform: translateX(-100%)
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav id="navbar">
+    <div v-for="(router, index) in routers" :key="index" class="navbar-item">
+      <router-link :to="router.to" @click="$emit('input', index)">
+        {{ router.label }}
+      </router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data() {
+    return {
+      routers: [
+        { label: "首頁", to: { name: "Home" } },
+        { label: "關於", to: { name: "About" } },
+        { label: "其他", to: { name: "Other" } },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+#navbar
+  background-color: #FFFFFF
+  display: flex
+  flex-flow: row nowrap
+  justify-content: flex-start
+  align-items: center
+  padding: 1rem 1rem
+.navbar-item
+  padding: 0 1.5rem
+  &:not(:last-child)
+    border-right: #C8C8C8 solid 1px
+</style>
